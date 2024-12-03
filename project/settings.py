f8de7b4ad79985fcd06e665995371d3046f844f9
carloscelestino1 <proglogic1@gmail.com>
2024-12-03 20:44:44 -0300

    Update settings.py
    incremento do host

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5^!*ec4)-ry^_36mf54w75!#4#zsbvp$$d)*ds%gopb_$sf=i8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mega-sena-janilson-cc27a92baa31.herokuapp.com']
 
 
 # Application definition
